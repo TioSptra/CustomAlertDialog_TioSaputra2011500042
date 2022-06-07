3c4275650eaceb4d4562543b6f8810d403107240
TioSptra <tiosptra@gmail.com>
2022-06-07 10:12:01 +0700

    tugas10 CAD

---
+++ app/src/main/java/com/example/customalertdialog_tiosaputra/Layout_alert.kt
@@ -0,0 +1,11 @@
+package com.example.customalertdialog_tiosaputra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Layout_alert : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_alert)
+    }
+}
